84313403e0b3bbe35a8c4cd16262a07032180e7b
Shakti <sh616913@dal.ca>
2018-02-10 14:56:29 -0400

    Basic Map activity added

--- app/src/main/java/com/example/shakt/baked/MainActivity.java
+++ app/src/main/java/com/example/shakt/baked/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.shakt.baked;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnMapActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMapActivity = findViewById(R.id.map_button);
+
+        btnMapActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent  = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
