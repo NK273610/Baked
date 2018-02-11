8d477bf89a0e99e4d2805c0619c1e553d55398b8
Shakti Singh <cpt.shakti.singh@gmail.com>
2018-02-10 21:23:21 -0400

    Basic Map activity added

--- app/src/main/java/com/example/shakt/baked/HomeActivity.java
+++ app/src/main/java/com/example/shakt/baked/HomeActivity.java
@@ -6,14 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     private Button btnMapActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         btnMapActivity = findViewById(R.id.map_button);
 
