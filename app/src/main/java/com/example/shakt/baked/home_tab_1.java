6f67dc23f1cf7a3a76215307a85deb6feced4aa5
Shakti <cpt.shakti.singh@gmail.com>
2018-03-12 01:18:13 -0300

    Converted home activity to a tabbed activity

---
+++ app/src/main/java/com/example/shakt/baked/home_tab_1.java
@@ -0,0 +1,28 @@
+package com.example.shakt.baked;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class home_tab_1 extends Fragment {
+
+
+    public home_tab_1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_tab_1, container, false);
+
+    }
+
+}
