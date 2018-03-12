a082ea85abbfd36e9b77db4ca93b2482f9e1ff46
Shakti <cpt.shakti.singh@gmail.com>
2018-03-12 14:05:41 -0300

    resolved conflicts with prashant's work

--- app/src/main/java/com/example/shakt/baked/home_tab_1.java
+++ app/src/main/java/com/example/shakt/baked/home_tab_1.java
@@ -1,16 +1,19 @@
 package com.example.shakt.baked;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class home_tab_1 extends Fragment {
+public class home_tab_1 extends Fragment implements View.OnClickListener{
 
+    private Button infoButton;
 
     public home_tab_1() {
         // Required empty public constructor
@@ -20,9 +23,17 @@ public class home_tab_1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_tab_1, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_tab_1, container, false);
+        infoButton = (Button) view.findViewById(R.id.info_button);
+        infoButton.setOnClickListener(this);
+        return view;
+    }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent  = new Intent(getActivity(), InfoActivity.class);
+        startActivity(intent);
     }
 
 }
