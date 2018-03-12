54530007596347ceaf3ca7ec716415f25f1144ac
Shakti <cpt.shakti.singh@gmail.com>
2018-03-12 19:14:55 -0300

    Home activity functionality added

--- app/src/main/java/com/example/shakt/baked/HomeActivity.java
+++ app/src/main/java/com/example/shakt/baked/HomeActivity.java
@@ -70,46 +70,46 @@ public class HomeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            if (getArguments().getInt(ARG_SECTION_NUMBER)==1){
-                View rootView = inflater.inflate(R.layout.fragment_home_tab_1, container, false);
-                return rootView;
-            }
-            else{
-                View rootView = inflater.inflate(R.layout.fragment_home_tab_2, container, false);
-                return rootView;
-            }
-
-        }
-    }
+//    /**
+//     * A placeholder fragment containing a simple view.
+//     */
+//    public static class PlaceholderFragment extends Fragment {
+//        /**
+//         * The fragment argument representing the section number for this
+//         * fragment.
+//         */
+//        private static final String ARG_SECTION_NUMBER = "section_number";
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        /**
+//         * Returns a new instance of this fragment for the given section
+//         * number.
+//         */
+//        public static PlaceholderFragment newInstance(int sectionNumber) {
+//            PlaceholderFragment fragment = new PlaceholderFragment();
+//            Bundle args = new Bundle();
+//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//            fragment.setArguments(args);
+//            return fragment;
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                                 Bundle savedInstanceState) {
+//
+//            if (getArguments().getInt(ARG_SECTION_NUMBER)==1){
+//                View rootView = inflater.inflate(R.layout.fragment_home_tab_1, container, false);
+//                return rootView;
+//            }
+//            else{
+//                View rootView = inflater.inflate(R.layout.fragment_home_tab_2, container, false);
+//                return rootView;
+//            }
+//
+//        }
+//    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -125,7 +125,18 @@ public class HomeActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+//            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    home_tab_1 tab1 = new home_tab_1();
+                    return tab1;
+                case 1:
+                    home_tab_2 tab2 = new home_tab_2();
+                    return tab2;
+                default:
+                    return null;
+            }
+
         }
 
         @Override
