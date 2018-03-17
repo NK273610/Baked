8e2bd47e2812cafd151b43ab023fb3d88f400e2b
Shakti <cpt.shakti.singh@gmail.com>
2018-03-17 01:07:58 -0300

    Link to user account activity from home page

--- app/src/main/java/com/example/shakt/baked/home_tab_1.java
+++ app/src/main/java/com/example/shakt/baked/home_tab_1.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 public class home_tab_1 extends Fragment implements View.OnClickListener{
 
     private Button infoButton;
+    private Button accountButton;
 
     public home_tab_1() {
         // Required empty public constructor
@@ -27,13 +28,25 @@ public class home_tab_1 extends Fragment implements View.OnClickListener{
         // Inflate the layout for this fragment
         infoButton = (Button) view.findViewById(R.id.info_button);
         infoButton.setOnClickListener(this);
+        accountButton = (Button) view.findViewById(R.id.account_button);
+        accountButton.setOnClickListener(this);
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent  = new Intent(getActivity().getApplicationContext(), InfoActivity.class);
-        startActivity(intent);
+
+        switch (v.getId()){
+            case R.id.account_button:
+                Intent intent1  = new Intent(getActivity().getApplicationContext(), UserAccount.class);
+                startActivity(intent1);
+                break;
+            case R.id.info_button:
+                Intent intent2  = new Intent(getActivity().getApplicationContext(), InfoActivity.class);
+                startActivity(intent2);
+                break;
+        }
+
     }
 
 }
